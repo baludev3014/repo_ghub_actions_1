4d2588fb84b08c082cceec6cacf8ab1e191b1272
baludev3014 <kbalu3013@gmail.com>
2026-03-04 23:56:01 +0530

    new route called route was added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/',(req,res)=>{
     res.send("Home")
 })
 
+app.get('/roite',(req,res)=>{
+    res.send("new route")
+})
 
 app.listen(4000,()=>{
     console.log("App is listening on port 4000")
